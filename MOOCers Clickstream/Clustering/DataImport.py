8c984d1cf22074960022a29fbbb955c1a333d869
Thilini <thilini.12@cse.mrt.ac.lk>
2016-12-13 14:54:54 +0530

    Adding_SR

--- MOOCers Clickstream/Clustering/DataImport.py
+++ MOOCers Clickstream/Clustering/DataImport.py
@@ -1,66 +1,109 @@
 import numpy as np
+from datetime import datetime, date
 import csv
+import pandas as pd
+from operator import itemgetter
 import itertools
 Video_code = ['i4x-Engineering-CS101-video-z68']
-f = open('C://Users//Kushan//Documents//MOOCers//MOOCers//MOOCers Clickstream//Clustering//Videos//'+Video_code[0]+'.csv')
-csv_f = csv.reader(f)
+file = 'C://Users//Kushan//Documents//MOOCers//MOOCers//MOOCers Clickstream//Clustering//Videos//'+Video_code[0]+'.csv'
 
+#reading video interaction file for video
+df = pd.read_csv(file, parse_dates=True)
+data = pd.DataFrame(df)
+
+#Create csv file to write data and add the headings
 s = open('C://Users//Kushan//Documents//MOOCers//MOOCers//MOOCers Clickstream//Clustering//Sessions//session.csv', 'w', newline='')
 csv_session = csv.writer(s)
-session = ['user_id','NP','NB','NF']
+session = ['user_id', 'NP', 'NB', 'NF', 'MP', 'SR']
 csv_session.writerow(session);
 
-#Getting N rows as the data set
-data = []
-#for row in itertools.islice(csv_f,1,10000):
-for row in csv_f:
-    data.append(row)
-
 #reading the unique student ids into a list
-student_ids = []
-for row in data:
-    if row[13] not in student_ids:
-        student_ids.append(row[13])
+student_ids = data.anon_screen_name.unique()
 
+i = 0
 #for each student create activity sequence
 for student in student_ids:
-    print(student);
-    activity_list = []
-    for row in data:
-        if row[13] == student:
-            activity_list.append(row)
-    activity_list.sort(key=lambda tup: tup[10])
+    i = i + 1
+
+    activity_list = data[data.anon_screen_name == student]
+    activity_list['time'] = activity_list['time'].apply(lambda x: datetime.strptime(x, '%M:%S.%f'))
+    activity_list = activity_list.sort_values(by='time')
+
+
+    prev_time = activity_list['time'].iloc[0]
+    for index, row in activity_list.iterrows():
+        time_now = row['time']
+        time_difference = time_now-prev_time
+        prev_time = time_now
+
+        if time_difference.total_seconds() >(30*60):
+            activity_list = activity_list[:index]
+
 
     #find the no of pauses NP
-    NP = sum(x[0] == 'pause_video' for x in activity_list)
-    print("NP: ", NP);
+    NP = len(activity_list[activity_list.event_type == 'pause_video'])
 
-    '''
+    pause_val = 0
+    play_val = 0
+    pause_detected = False
+
     #Median duration of pauses
-    pause_duration = []
-    for row in activity_list:
-        if row[0] == 'pause_video':
-    '''
 
-    #no of forward seeks
-    NB = sum(x[0]=='seek_video' and x[6]<x[7] for x in activity_list)
-    NF = sum(x[0] == 'seek_video' and x[6] > x[7] for x in activity_list)
-    print("NB: ",NB);
-    print("NF: ",NF);
+    pause_duration = []
+    for index, row in activity_list.iterrows():
+        if row['event_type'] == 'play_video':
+            play_val = row['time']
+            if pause_detected:
+                #play_val = datetime.strptime(play_val, "%M:%S.%f")
+                #pause_val = datetime.strptime(pause_val, "%M:%S.%f")
+                time_diff = play_val - pause_val
+                if time_diff.total_seconds() < 0:
+                    print(student)
+                    print(play_val)
+                    print(pause_val)
+                pause_duration.append(time_diff.total_seconds())
+                pause_detected = False
+        elif row['event_type'] == 'pause_video':
+            pause_val = row['time']
+            pause_detected = True
+
+    MP = np.median(pause_duration)
+
+    #proportion of skipped video content
+
+    seek_forward_list = activity_list.loc[(activity_list.event_type == 'seek_video') & (activity_list.video_new_time > activity_list.video_old_time)]
+
+    seek_forward_list['SR'] = seek_forward_list['video_new_time'] - seek_forward_list['video_old_time']
+
+    SR = seek_forward_list['SR'].sum()
+
+    #no of forward seeks and backward seeks
+    NB = len(activity_list[(activity_list.event_type == 'seek_video') & (activity_list.video_new_time < activity_list.video_old_time)])
+    NF = len(activity_list[(activity_list.event_type == 'seek_video') & (activity_list.video_new_time > activity_list.video_old_time)])
 
     session = []
     session.append(student)
     session.append(NP)
     session.append(NB)
     session.append(NF)
-    csv_session.writerow(session);
+    session.append(MP)
+    session.append(SR)
+    csv_session.writerow(session)
 
+    print((i/len(student_ids))*100)
+
+    '''
     w = 'C://Users//Kushan//Documents//MOOCers//MOOCers//MOOCers Clickstream//Clustering//Videos//'+(student)+'.csv'
+    print(student)
 
     with open(w, 'w', newline='') as mycsvfile:
         thedatawriter = csv.writer(mycsvfile)
-        for row in activity_list:
+        for index, row in activity_list.iterrows():
             thedatawriter.writerow(row)
+    '''
+
+
+
 
 
 
--- MOOCers Clickstream/Clustering/K-means.py
+++ MOOCers Clickstream/Clustering/K-means.py
@@ -1,47 +1,100 @@
 import csv
 import pandas as pd
+from numpy import *
 import numpy as np
 import pylab as pl
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 from sklearn import cluster
+from scipy.spatial.distance import cdist, pdist
+
+from sklearn.metrics import silhouette_samples, silhouette_score
 
 file = 'C://Users//Kushan//Documents//MOOCers//MOOCers//MOOCers Clickstream//Clustering//Sessions//session.csv'
 
 df = pd.read_csv(file, parse_dates=True)
-data = pd.DataFrame(df, columns=('NP', 'NB', 'NF'))
+data = pd.DataFrame(df, columns=('NP', 'NB', 'NF', 'MP', 'SR'))
 
 original_length = len(data)
 
-data = data[(data['NP'] > 0) | (data['NB'] > 0) | (data['NF'] > 0)]
+#data = data[(data['NP'] > 0) | (data['NB'] > 0) | (data['NF'] > 0)]
+#print("Removed percentage: ", ((original_length-len(data))/original_length)*100)
 
-print("Removed percentage: ", ((original_length-len(data))/original_length)*100)
+nan_positions = isnan(data['MP'])
+data[nan_positions] = 0
 
-k_means = cluster.KMeans(n_clusters=5, random_state=0).fit(data)
 
-centroids = k_means.cluster_centers_
-cluster_labels = k_means.labels_
+#normalizing the columns
+data_norm = (data - data.min()) / (data.max() - data.min())
+
+'''
+#silhoutte criterion graph
+range_n_clusters = [2, 3, 4, 5, 6, 7]
+silhouette_avg_list = []
+
+for n_clusters in range_n_clusters:
+    k_means = cluster.KMeans(n_clusters=n_clusters, random_state=10).fit(data_norm)
+    centroids = k_means.cluster_centers_
+    cluster_labels = k_means.labels_
+    silhouette_avg = silhouette_score(data_norm, cluster_labels,metric='manhattan')
+    silhouette_avg_list.append(silhouette_avg)
+    print("For n_clusters =", n_clusters,"The average silhouette_score is :", silhouette_avg)
+
 
 fig = plt.figure()
 ax = fig.add_subplot(111)
-scatter = ax.scatter(data['NP'], data['NB'], c=cluster_labels)
-scatter = ax.scatter(centroids[:, 0], centroids[:, 1], c="Red", marker="x")
-
-ax.set_xlabel('x')
-ax.set_ylabel('y')
-
+ax.plot(range_n_clusters, silhouette_avg_list)
 plt.show()
+'''
 
 '''
-threedee = plt.figure().gca(projection='2d')
-threedee.scatter(data['NF'], data['NP'], c=k_means.labels_)
+#elbow method
+n = 10
+kMeansVar = [cluster.KMeans(n_clusters=k).fit(data_norm) for k in range(1, n)]
+centroids = [X.cluster_centers_ for X in kMeansVar]
+k_euclid = [cdist(data_norm, cent) for cent in centroids]
+dist = [np.min(ke, axis=1) for ke in k_euclid]
+wcss = [sum(d**2) for d in dist]
+tss = sum(pdist(data_norm)**2)/data_norm.shape[0]
+bss = tss - wcss
+plt.plot(range(1, n), bss)
+plt.show()
+'''
+k_means = cluster.KMeans(n_clusters=2, random_state=10).fit(data_norm)
+centroids = k_means.cluster_centers_
+cluster_labels = k_means.labels_
+
+i=0
+j=0
+for row in cluster_labels:
+    if row == 0:
+        i = i +1
+    if row == 1:
+        j = j +1
+
+print("Cluster 0 :", i)
+print("Cluster 1 :", j)
+
+fig = plt.figure()
+ax = fig.add_subplot(111)
+scatter = ax.scatter(data_norm['SR'], data_norm['NF'], c=cluster_labels)
+scatter = ax.scatter(centroids[:, 0], centroids[:, 1], c="Red", marker="x")
+ax.set_xlabel('NF')
+ax.set_ylabel('MP')
+print(centroids)
+plt.show()
+
+
+'''
+threedee = plt.figure().gca(projection='3d')
+threedee.scatter(data_norm['NF'], data['NP'],data_norm['NB'], c=k_means.labels_)
 threedee.scatter(k_means.cluster_centers_[:, 0], k_means.cluster_centers_[:, 1],c="Red", marker='x')
 threedee.set_xlabel('NF')
 threedee.set_ylabel('NP')
 threedee.set_zlabel('NB')
 plt.show()
-'''
 
+'''
 
 
 
