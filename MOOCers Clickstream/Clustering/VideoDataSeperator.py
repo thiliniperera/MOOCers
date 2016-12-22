324bdfcef0eafdeb9a670ebd13eb846d8ff0bfa4
kasubdn <kasun.bdn@gmail.com>
2016-12-22 12:42:08 +0530

    configurations

--- MOOCers Clickstream/Clustering/VideoDataSeperator.py
+++ MOOCers Clickstream/Clustering/VideoDataSeperator.py
@@ -1,13 +1,18 @@
 import pandas as pd
 import numpy as np
 
+from settings import Configurations
+
 #f = open('C://Users//Kushan//Documents//Data//Chamil//HumanitiesandScience_StatLearning_Winter2015//HumanitiesandScience_StatLearning_Winter2015_VideoInteraction.csv')
-f = open('C://Users//Kushan//Documents//Data//Data//Engineering_CS101_Summer2014//Engineering_CS101_Summer2014_VideoInteraction.csv')
+f = open(Configurations.InitialVideoInteractionFile[0])
 df = pd.read_csv(f, parse_dates=True, dtype={'video_old_speed': np.float64, 'video_new_speed': np.float64})
 
+for i in range(0,len(df.columns.values)):
+    df.columns.values[i] = df.columns.values[i].replace("'", "")
+
 #Video_code = ['i4x-HumanitiesandScience-StatLearning-video-de1971b8a61e45d584364679e5e07e55']
-Video_code = ['i4x-Engineering-CS101-video-3f5301fa02fd4b60a541f1497eb3ff64']
-Video = df[df.video_id == Video_code[0]]
+Video_code = Configurations.Video_code[0]
+Video = df[df['video_id'] == Video_code[0]]
 
 print("Writing csv......")
 w = ('videos/'+Video_code[0])+'.csv'
---
+++ MOOCers Clickstream/Clustering/settings.py
@@ -0,0 +1,12 @@
+# This file contains the global variables and constants
+
+class Configurations:
+    #thilini
+    #Video_code = ['i4x-Engineering-CS101-video-3f5301fa02fd4b60a541f1497eb3ff64']
+    #InitialVedioInteractionFile = ['C://Users//Kushan//Documents//Data//Data//Engineering_CS101_Summer2014//Engineering_CS101_Summer2014_VideoInteraction.csv']
+
+
+
+    #kasun
+    Video_code = ['i4x-Engineering-CS101-video-z68']
+    InitialVideoInteractionFile = ['/home/kasun/Clustering/Engineering_CS101_Summer2014_VideoInteraction.csv']
