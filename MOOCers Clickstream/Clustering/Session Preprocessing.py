2038c7f503ecdfd01526bcad9cc5242351a082ab
Thilini <thilini.12@cse.mrt.ac.lk>
2017-01-04 09:22:32 +0530

    loading file in chunks

--- MOOCers Clickstream/Clustering/Session Preprocessing.py
+++ MOOCers Clickstream/Clustering/Session Preprocessing.py
@@ -1,21 +1,27 @@
 import pandas as pd
 from numpy import *
 import matplotlib.pyplot as plt
-Video_code = ['i4x-Engineering-CS101-video-3f5301fa02fd4b60a541f1497eb3ff64']
+from settings import Configurations
+
+Video_code = Configurations.Video_code
+
 file = 'sessions/session_'+Video_code[0]+'.csv'
 
 df = pd.read_csv(file, parse_dates=True)
-data = pd.DataFrame(df, columns=('NP', 'NB', 'NF', 'MP', 'SR', 'RL', 'AS', 'ES', 'TP', 'session_no'))
+data = pd.DataFrame(df, columns=('NP', 'NB', 'NF', 'MP', 'SR', 'AS','RL','TP', 'ES', 'session_no'))
 
 nan_positions = isnan(data['MP'])
 data[nan_positions] = 0
 
 #data = data[(data.NP != 100) & (data.NB <= 40) & (data.NF <= 40)] # Removing outliers
-data = data[data.TP >100]
+data = data[data.TP > 100]
+data = data[data.TP < 2000]
 print(data.describe())
 plt.figure()
-data.hist()
+data['TP'].hist()
 plt.show()
 
-s = open('preprocessed_session'+Video_code[0]+'.csv', 'w', newline='')
+print(len(data))
+
+s = open('preprocessed_session'+Video_code[0]+'.csv', 'w')
 data.to_csv(s, index=False)
