b88864fbd1edcb504b2b1a2ec3198cfbaca0e41b
chamil321 <chamilmadawa8@gmail.com>
2017-01-25 10:31:47 +0530

    Convert R to python

---
+++ AssessmentDropout/assessment_prediction/assessmentDataGathering.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import numpy as np
+
+from resourses import Project_resourses
+
+file_activityGrade = open(Project_resourses.activityGradeFile)
+print file_activityGrade
---
+++ AssessmentDropout/assessment_prediction/resourses.py
@@ -0,0 +1,4 @@
+class Project_resourses:
+
+    activityGradeFile = "/media/chamil/New Volume/My Documents/1.ENGI/Semester 8/FYP/EDUC115/Education_EDUC115-S_Spring2014_ActivityGrade.csv"
+    finalGradeFile = "/media/chamil/New Volume/My Documents/1.ENGI/Semester 8/FYP/EDUC115/Education_EDUC115-S_Spring2014_FinalGrade.csv "
