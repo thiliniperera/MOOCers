6bc6fbc1a3857d569f40c26e143ac038a0d28aca
kasubdn <kasun.bdn@gmail.com>
2017-01-28 08:51:44 +0530

    user vs commentThread matrix complete

--- ForumAnalysis/ForumAnalysis.py
+++ ForumAnalysis/ForumAnalysis.py
@@ -1,4 +1,6 @@
+from __future__ import division
 import pandas as pd
+import numpy
 
 from Settings import Configurations
 
@@ -6,15 +8,32 @@ initialFile = open(Configurations.InitialForumDataFile[0])
 
 df = pd.read_csv(initialFile)
 uniqueUsers = df['anon_screen_name'].unique()
-comentThreadIDs =  df[df['type'] == 'CommentThread']['forum_post_id'].unique()
+users = df['anon_screen_name'].values
+uniqueCommentThreadIDs =  df[df['type'] == 'CommentThread']['forum_post_id'].unique()
 
-z = df['forum_post_id'].where(df['type'] == 'CommentThread', df['comment_thread_id'])
+commentThreadIDs = df['forum_post_id'].where(df['type'] == 'CommentThread', df['comment_thread_id']).values
 
-df['forum_id'] = z
+# df['forum_id'] = commentThreadIDs
+xDim= len(uniqueUsers)
+yDim= len(uniqueCommentThreadIDs)
+totalLoops = xDim*yDim
+print xDim
+print yDim
+print len(commentThreadIDs)
 
-initialMatrix = [[0 for x in range(len(comentThreadIDs))] for y in range(len(uniqueUsers))]
+initialMatrix = [[0 for x in range(len(uniqueCommentThreadIDs))] for y in range(len(uniqueUsers))]
 
 for i in range(len(uniqueUsers)):
-    for j in range(len(comentThreadIDs)):
-        initialMatrix[i][j]=1
+    for j in range(len(uniqueCommentThreadIDs)):
+        involvement = 0
+        for k in range(len(commentThreadIDs)):
+            if(uniqueUsers[i]==users[k] and uniqueCommentThreadIDs[j]==commentThreadIDs[k]):
+                involvement=involvement+1
+
+        initialMatrix[i][j]=involvement if involvement<3 else min(2,involvement)
+        # print (i*yDim+j)*100/totalLoops
+
+
+
+print initialMatrix
 
