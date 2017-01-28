8393b88b66cad0d6bdf421b0438ab732443355f5
kasubdn <kasun.bdn@gmail.com>
2017-01-28 09:20:00 +0530

    adj matrix complete

--- ForumAnalysis/ForumAnalysis.py
+++ ForumAnalysis/ForumAnalysis.py
@@ -1,12 +1,12 @@
 #from __future__ import division
 import pandas as pd
-import numpy
+import numpy as np
 
 from Settings import Configurations
 
 initialFile = open(Configurations.InitialForumDataFile[0])
 
-df = pd.read_csv(initialFile)
+df = pd.read_csv(initialFile, nrows=1000)
 uniqueUsers = df['anon_screen_name'].unique()
 users = df['anon_screen_name'].values
 uniqueCommentThreadIDs =  df[df['type'] == 'CommentThread']['forum_post_id'].unique()
@@ -21,7 +21,7 @@ print xDim
 print yDim
 print len(commentThreadIDs)
 
-initialMatrix = [[0 for x in range(len(uniqueCommentThreadIDs))] for y in range(len(uniqueUsers))]
+initialArray = [[0 for x in range(len(uniqueCommentThreadIDs))] for y in range(len(uniqueUsers))]
 
 for i in range(len(uniqueUsers)):
     for j in range(len(uniqueCommentThreadIDs)):
@@ -30,10 +30,13 @@ for i in range(len(uniqueUsers)):
             if(uniqueUsers[i]==users[k] and uniqueCommentThreadIDs[j]==commentThreadIDs[k]):
                 involvement=involvement+1
 
-        initialMatrix[i][j]=involvement if involvement<3 else min(2,involvement)
+        initialArray[i][j]=involvement if involvement < 3 else min(2, involvement)
         print (i*yDim+j)*100/totalLoops
 
 
 
-print initialMatrix
+initialMatrix= np.asmatrix(initialArray)
+adjMatrix = initialMatrix.T.dot(initialMatrix)
+
+print adjMatrix
 
