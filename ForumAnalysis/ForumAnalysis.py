e713c4086932d85c0d5da893da91dd0d52a51417
kasubdn <kasun.bdn@gmail.com>
2017-01-28 07:32:20 +0530

    initial matrix creation

--- ForumAnalysis/ForumAnalysis.py
+++ ForumAnalysis/ForumAnalysis.py
@@ -8,6 +8,9 @@ df = pd.read_csv(initialFile)
 uniqueUsers = df['anon_screen_name'].unique()
 comentThreadIDs =  df[df['type'] == 'CommentThread']['forum_post_id'].unique()
 
+z = df['forum_post_id'].where(df['type'] == 'CommentThread', df['comment_thread_id'])
+
+df['forum_id'] = z
 
 initialMatrix = [[0 for x in range(len(comentThreadIDs))] for y in range(len(uniqueUsers))]
 
@@ -15,5 +18,3 @@ for i in range(len(uniqueUsers)):
     for j in range(len(comentThreadIDs)):
         initialMatrix[i][j]=1
 
-
-print
